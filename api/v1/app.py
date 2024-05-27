6e3fe84bfc60c8c41ec87341145d8a205eaa2152
glovic <xtineviktor@gmail.com>
2024-05-27 13:58:59 +0100

    api update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,52 +1,45 @@
 #!/usr/bin/python3
+"""
+app
+"""
 
-"""This module contains routes for the app."""
-
-import os
 from flask import Flask, jsonify
 from flask_cors import CORS
-from models import storage
+from os import getenv
+
 from api.v1.views import app_views
+from models import storage
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.register_blueprint(app_views)
 
-# add cors support
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close(_):
-    """Handles session teardown operation."""
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(_):
-    """Returns an error 404 for page not found errors."""
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(400)
-def bad_request(error):
+def handle_404(exception):
     """
-    Handles the 400 Bad Request error.
-
-    Args:
-        error (Exception): The error object representing the 400 Bad Request.
-
-    Returns:
-        tuple: A tuple containing the error message and the HTTP status code
-        400.
+    handles 404 error
+    :return: returns 404 json
     """
-    return str(error), 400
+    data = {
+        "error": "Not found"
+    }
 
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
 
 if __name__ == "__main__":
-    app.run(
-        host=os.getenv('HBNB_API_HOST', default='0.0.0.0'),
-        port=int(os.getenv('HBNB_API_PORT', default='5000')),
-        threaded=True, debug=True
-    )
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
